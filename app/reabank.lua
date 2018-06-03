jtackaberry/reaticulate

commit bf4609b19d43d774bf83bc06a5132e9b2d97dee8

Regenerate tmp reabank if it's missing

app/reabank.lua
@@ -729,7 +729,7 @@ function reabank.init()
     local cur_factory_bank_size, err = file_size(reabank.reabank_filename_factory)
     local file = get_reabank_file() or ''
     local tmpnum = file:lower():match("-tmp(%d+).")
-    if tmpnum then
+    if tmpnum and file_exists(file) then
         log("tmp rebeank exists: %s", file)
         reabank.version = tonumber(tmpnum)
         reabank.filename_tmp = file

app/lib/utils.lua
@@ -90,6 +90,16 @@ function file_size(fname)
     end
 end
 
+function file_exists(fname)
+    local f, err = io.open(fname)
+    if f then
+        f:close()
+        return true
+    else
+        return false
+    end
+end
+
 function table.val_to_str(v)
     if "string" == type(v) then
         v = string.gsub(v, "\n", "\\n")

commit 682a5602fa43c28efe52ca5ff8d6ded90cf1b344

Fix bank menu
Broke in bf4609b1

Reaticulate-factory.reabank
@@ -4053,7 +4053,7 @@ Bank 66 77 Albion 3 - Siege of Strings
 // XXX: This bank is deprecated (replaced by note-based banks 70 4/5) and so it
 // is hidden by default.
 //
-//! g="Cinematic Series" n="Studio Strings"
+//! g="Cinematic Series/Studio Strings" n="(Previous version - deprecated)"
 //! chase=1,2,5,11,20,64-69 f=hidden
 Bank 70 1 Cinematic Studio Strings
 //! c=legato i=legato g=2 o=cc:58,76

app/reabank.lua
@@ -839,7 +839,7 @@ function reabank.to_menu()
         submenu[#submenu+1] = {
             bank.shortname or bank.name,
             tostring(bank.msblsb),
-            bank.hidden and rtk.OptionMenu.ITEM_HIDDEN or rtk.OptionMenu.ITEM_NORMAL,
+            bank.hidden and rtk.OptionMenu.ITEM_DISABLED or rtk.OptionMenu.ITEM_NORMAL,
             bank.name
         }
     end

banks/70-01-Cinematic_Series-Cinematic_Studio_Strings.reabank
@@ -2,7 +2,7 @@
 // XXX: This bank is deprecated (replaced by note-based banks 70 4/5) and so it
 // is hidden by default.
 //
-//! g="Cinematic Series" n="Studio Strings"
+//! g="Cinematic Series/Studio Strings" n="(Previous version - deprecated)"
 //! chase=1,2,5,11,20,64-69 f=hidden
 Bank 70 1 Cinematic Studio Strings
 //! c=legato i=legato g=2 o=cc:58,76