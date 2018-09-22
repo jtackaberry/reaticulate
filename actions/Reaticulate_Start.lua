78e984fe9d4435bd8e5dabf83fcdf3b6af961cac
Jason Tackaberry <tack@urandom.ca>
2018-09-22 16:34:48 -0400

    Improve robustness if Reaticulate_Main action can't be found
    Hopefully improves first-run experience for users on non-default
    installations.
    
    Fixes #46.

--- actions/Reaticulate_Start.lua
+++ actions/Reaticulate_Start.lua
@@ -6,7 +6,7 @@ reaper.SetExtState("reaticulate", "command", "ping=" .. now, false)
 local attempts = 2
 function check()
     val = reaper.GetExtState("reaticulate", "pong")
-    if val == now then
+    if val >= now then
         -- Reaticulate is running.
         return
     end
@@ -17,11 +17,20 @@ function check()
     else
         -- Lookup cmd id saved from previous invocation of Reaticulate_Main.
         cmd = reaper.GetExtState("reaticulate", "main_command_id")
-        if not cmd or not reaper.ReverseNamedCommandLookup(tonumber(cmd)) then
+        if cmd == '' or not cmd or not reaper.ReverseNamedCommandLookup(tonumber(cmd)) then
             -- This is the command id for the default script location
             cmd = reaper.NamedCommandLookup('_RSbe259504561f6a52557d2d1c64e52ef13527bf17')
         end
-        reaper.Main_OnCommandEx(tonumber(cmd), 0, 0)
+        if cmd == '' or not cmd or cmd == 0 then
+            reaper.ShowMessageBox(
+                "Couldn't open Reaticulate.  This is due to a REAPER limitation.\n\n" ..
+                "Workaround: open REAPER's actions list and manually run Reaticulate_Main.\n\n" ..
+                "You will only need to do this once.",
+                "Reaticulate: Error", 0
+            )
+        else
+            reaper.Main_OnCommandEx(tonumber(cmd), 0, 0)
+        end
     end
 end
 
