jtackaberry/reaticulate

commit f93ecd99c5a491bfb546166e9cdf865305fe80e8

Screens now use their own individual viewports

app/screens/banklist.lua
@@ -238,13 +238,17 @@ function screen.show_track_banks()
     end
 end
 
+function screen.focus_filter()
+    screen.filter_entry:focus()
+    screen.filter_refocus_on_activation = not rtk.in_window
+    return rtk.focus()
+end
 
 function screen.init()
     screen.widget = rtk.VBox:new()
 
     local topbar = rtk.VBox:new({
         spacing=0,
-        position=rtk.Widget.FIXED,
         bg=rtk.theme.window_bg,
         y=0,
         focusable=true,
@@ -294,7 +298,9 @@ function screen.init()
             topbar:add(row, {tpadding=0, halign=rtk.Widget.CENTER})
         end
     end
-    screen.banks = screen.widget:add(rtk.VBox:new({bpadding=20}))
+    screen.banks = rtk.VBox({bpadding=20})
+    screen.viewport = rtk.Viewport({child=screen.banks})
+    screen.widget:add(screen.viewport, {fillw=true})
 
     -- Info / button when no banks are configured on track (hidden when there are banks)
     screen.no_banks_box = rtk.VBox:new()
@@ -314,10 +320,6 @@ function screen.init()
     })
     screen.no_banks_box:add(button, {halign=rtk.Widget.CENTER, tpadding=20})
     button.onclick = track_button.onclick
-
-    -- Testing nested viewports
-    -- screen.widget = rtk.Viewport:new({child=screen.widget}  )
-    -- screen.widget:scrollto(0, 200)
 end
 
 -- Ensures the default channel is highlighted.  Called by the main app when the default

app/screens/settings.lua
@@ -45,10 +45,10 @@ end
 
 local function make_section(title)
     local heading = rtk.Heading:new({label=title})
-    screen.widget:add(heading, {
+    screen.vbox:add(heading, {
         lpadding=10, tpadding=30, bpadding=15
     })
-    return screen.widget:add(rtk.VBox:new({spacing=10, lpadding=20, bpadding=0}))
+    return screen.vbox:add(rtk.VBox:new({spacing=10, lpadding=20, bpadding=0}))
 end
 
 local function add_row(section, label, w, spacing)
@@ -61,7 +61,8 @@ local function add_row(section, label, w, spacing)
 end
 
 function screen.init()
-    screen.widget = rtk.VBox:new()
+    screen.vbox = rtk.VBox()
+    screen.widget = rtk.Viewport({child=screen.vbox})
     screen.toolbar = rtk.HBox:new({spacing=0})
 
     -- Back button: return to bank list

app/screens/trackcfg.lua
@@ -43,7 +43,8 @@ end
 function screen.init()
     screen.error_icon = rtk.Image:new(Path.join(Path.imagedir, "warning_amber_24x24.png"))
     screen.info_icon = rtk.Image:new(Path.join(Path.imagedir, "info_outline_white_24x24.png"))
-    screen.widget = rtk.VBox:new()
+    local vbox = rtk.VBox:new()
+    screen.widget = rtk.Viewport({child=vbox, rpadding=10})
 
     screen.toolbar = rtk.HBox:new({spacing=0})
 
@@ -57,11 +58,11 @@ function screen.init()
     screen.toolbar:add(back_button)
 
     local heading = rtk.Heading:new({label="Track Articulations"})
-    screen.widget:add(heading, {
+    vbox:add(heading, {
         lpadding=10, tpadding=10, bpadding=20
     })
 
-    screen.banklist = screen.widget:add(rtk.VBox:new({spacing=10}), {lpadding=10})
+    screen.banklist = vbox:add(rtk.VBox:new({spacing=10}), {lpadding=10})
 
     local spacer = rtk.Spacer({h=150, w=1.0, y=0, z=10})
     spacer.ondropfocus = function(self, event, _, srcbankbox)
@@ -69,7 +70,7 @@ function screen.init()
         return true
     end
 
-    screen.widget:add(spacer, {tpadding=-20, bpadding=-130})
+    vbox:add(spacer, {tpadding=-20, bpadding=-130})
 
     local add_bank_button = app:make_button("add_circle_outline_white_18x18.png", "Add Bank", true)
     add_bank_button.onclick = function()
@@ -83,7 +84,7 @@ function screen.init()
             bankbox.bank_menu.onchange()
         end
     end
-    screen.widget:add(add_bank_button, {lpadding=20, tpadding=20, bpadding=40})
+    vbox:add(add_bank_button, {lpadding=20, tpadding=20, bpadding=40})
     screen.update()
 end
 
@@ -286,7 +287,7 @@ function screen.check_errors()
 end
 
 function screen.update()
-    app.viewport:scrollto(0, 0)
+    screen.widget:scrollto(0, 0)
     screen.banklist:clear()
     for srcchannel, dstchannel, msb, lsb in rfx.get_banks() do
         local bankbox = screen.create_bank_ui()

commit 38a9ad93c492ff1bcbb13bf3c0451cc051ebd902

Fix "no banks on track" vbox
Broke in f93ecd9

app/screens/banklist.lua
@@ -232,8 +232,10 @@ function screen.show_track_banks()
     end
     screen.visible_banks = visible
     if #visible > 0 then
+        screen.viewport:show()
         screen.no_banks_box:hide()
     else
+        screen.viewport:hide()
         screen.no_banks_box:show()
     end
 end
@@ -311,7 +313,7 @@ function screen.init()
     -- Info / button when no banks are configured on track (hidden when there are banks)
     screen.no_banks_box = rtk.VBox:new()
     screen.widget:add(screen.no_banks_box, {
-        halign=rtk.Widget.CENTER, valign=rtk.Widget.CENTER, tpadding=20
+        halign=rtk.Widget.CENTER, valign=rtk.Widget.CENTER, expand=1
     })
     local label = rtk.Label:new({
         label="No banks on this track", fontsize=24,