3188f2a2ad751ba2b6c4cbb4d65ce422fcca7fa3
Jason Tackaberry <tack@urandom.ca>
2019-06-19 19:40:48 -0400

    Fix center alignment in boxes
    Fixes regression introduced in bba40882

--- app/lib/rtk.lua
+++ app/lib/rtk.lua
@@ -2356,7 +2356,7 @@ function rtk.VBox:_reflow_step2(w, h, maxw, maxh, expand_unit_size, viewport)
                 -- to adjust position.
                 local offx = self.lpadding + lpadding
                 if attrs.halign == rtk.Widget.CENTER then
-                    offx = (offx - self.rpadding - rpadding) + (maxw - widget.cw) / 2
+                    offx = (offx - rpadding) + (maxw - widget.cw) / 2
                 elseif attrs.halign == rtk.Widget.RIGHT then
                     offx = offx + maxw - widget.cw
                 end
@@ -2449,7 +2449,7 @@ function rtk.HBox:_reflow_step2(w, h, maxw, maxh, expand_unit_size, viewport)
                 -- to adjust position.
                 local offy = self.tpadding + tpadding
                 if attrs.valign == rtk.Widget.CENTER then
-                    offy = (offy - self.bpadding - bpadding) + (maxh - widget.ch) / 2
+                    offy = (offy - bpadding) + (maxh - widget.ch) / 2
                 elseif attrs.valign == rtk.Widget.BOTTOM then
                     offy = offy + maxh - widget.ch
                 end

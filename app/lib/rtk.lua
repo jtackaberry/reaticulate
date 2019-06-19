jtackaberry/reaticulate

commit bba40882765c2503fb2ed09b634d0f9447b53a3d

Take another stab at fixing statusbar for rtk.Container screens

app/lib/rtk.lua
@@ -2348,7 +2348,7 @@ function rtk.VBox:_reflow_step2(w, h, maxw, maxh, expand_unit_size, viewport)
                 -- to adjust position.
                 local offx = self.lpadding + lpadding
                 if attrs.halign == rtk.Widget.CENTER then
-                    offx = offx + (maxw - widget.cw) / 2
+                    offx = (offx - self.rpadding - rpadding) + (maxw - widget.cw) / 2
                 elseif attrs.halign == rtk.Widget.RIGHT then
                     offx = offx + maxw - widget.cw
                 end
@@ -2441,7 +2441,7 @@ function rtk.HBox:_reflow_step2(w, h, maxw, maxh, expand_unit_size, viewport)
                 -- to adjust position.
                 local offy = self.tpadding + tpadding
                 if attrs.valign == rtk.Widget.CENTER then
-                    offy = offy + (maxh - widget.ch) / 2
+                    offy = (offy - self.bpadding - bpadding) + (maxh - widget.ch) / 2
                 elseif attrs.valign == rtk.Widget.BOTTOM then
                     offy = offy + maxh - widget.ch
                 end

app/lib/baseapp.lua
@@ -108,7 +108,7 @@ function BaseApp:show_screen(screen)
         if self.viewport then
             self.viewport:attr('child', screen.widget)
         else
-            self.frame:replace(self.frame.content_position, screen.widget, {expand=1, fillw=true})
+            self.frame:replace(self.frame.content_position, screen.widget, {expand=1, fillw=true, fillh=true})
         end
         if screen.toolbar then
             screen.toolbar:show()
@@ -287,6 +287,10 @@ function BaseApp:build_frame()
     self.frame = rtk.VBox:new({position=rtk.Widget.FIXED, z=100})
     self.frame:add(toolbar)
 
+    -- Add a placeholder widget that screens will replace.
+    self.frame:add(rtk.VBox.FLEXSPACE)
+    self.frame.content_position = #self.frame.children
+
     self.statusbar = rtk.HBox:new({
         bg=rtk.theme.window_bg,
         lpadding=10,
@@ -297,14 +301,10 @@ function BaseApp:build_frame()
     })
     self.statusbar.label = self.statusbar:add(rtk.Label:new({color=rtk.theme.text_faded}), {expand=1})
 
-    -- Add a placeholder widget that screens will replace.
-    self.frame:add(rtk.VBox.FLEXSPACE)
-    self.frame.content_position = #self.frame.children
-
-    self:set_statusbar('Reaticulate')
-
+    self.frame:add(self.statusbar)
     rtk.widget:add(self.frame)
-    rtk.widget:add(self.statusbar, {valign=rtk.Widget.BOTTOM, fillw=true})
+
+    self:set_statusbar('')
 end
 
 function BaseApp:handle_onupdate()

commit 3188f2a2ad751ba2b6c4cbb4d65ce422fcca7fa3

Fix center alignment in boxes
Fixes regression introduced in bba40882

app/lib/rtk.lua
@@ -2356,7 +2356,7 @@ function rtk.VBox:_reflow_step2(w, h, maxw, maxh, expand_unit_size, viewport)
                 -- to adjust position.
                 local offx = self.lpadding + lpadding
                 if attrs.halign == rtk.Widget.CENTER then
-                    offx = (offx - self.rpadding - rpadding) + (maxw - widget.cw) / 2
+                    offx = (offx - rpadding) + (maxw - widget.cw) / 2
                 elseif attrs.halign == rtk.Widget.RIGHT then
                     offx = offx + maxw - widget.cw
                 end
@@ -2449,7 +2449,7 @@ function rtk.HBox:_reflow_step2(w, h, maxw, maxh, expand_unit_size, viewport)
                 -- to adjust position.
                 local offy = self.tpadding + tpadding
                 if attrs.valign == rtk.Widget.CENTER then
-                    offy = (offy - self.bpadding - bpadding) + (maxh - widget.ch) / 2
+                    offy = (offy - bpadding) + (maxh - widget.ch) / 2
                 elseif attrs.valign == rtk.Widget.BOTTOM then
                     offy = offy + maxh - widget.ch
                 end