e1bcfbd3bf164322ef0bb56b1d67ff5253c110e3
Jason Tackaberry <tack@urandom.ca>
2019-10-23 18:42:29 -0400

    Fix ctrl/alt key detection

--- app/lib/rtk.lua
+++ app/lib/rtk.lua
@@ -366,25 +366,18 @@ function rtk.update()
     local char = gfx.getchar()
     if char > 0 then
         event = rtk._event:reset(rtk.Event.KEY)
+        event:set_modifiers(gfx.mouse_cap, 0)
         event.char = nil
         event.keycode = char
-        if char <= 26 then
+        if char <= 26 and event.ctrl then
             event.char = string.char(char + 96)
-            event.ctrl = true
-            event.alt = false
-        elseif char >= 32 then
+        elseif char >= 32 and char ~= 127 then
             if char <= 255 then
                 event.char = string.char(char)
-                event.ctrl = false
-                event.alt = false
             elseif char <= 282 then
                 event.char = string.char(char - 160)
-                event.ctrl = true
-                event.alt = true
             elseif char <= 346 then
                 event.char = string.char(char - 224)
-                event.ctrl = false
-                event.alt = true
             end
         end
         rtk.onkeypresspre(event)
