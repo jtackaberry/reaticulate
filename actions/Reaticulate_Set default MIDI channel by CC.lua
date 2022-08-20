55eb718cf45c830a06648bb7d6c060735379d796
Jason Tackaberry <tack@urandom.ca>
2022-08-20 15:46:06 -0400

    Merge pull request #189 from Kabraxist/osc_to_cc
    Handle 14-bit OSC values for Actions with CC input

--- actions/Reaticulate_Set default MIDI channel by CC.lua
+++ actions/Reaticulate_Set default MIDI channel by CC.lua
@@ -1,2 +1,6 @@
-_, _, _, _, _, _, val = reaper.get_action_context()
+_, _, _, _, _, res, val = reaper.get_action_context()
+
+--Simple check if the message is 14-bit or 7-bit
+if res == 16383 then val = 16384-val end
+
 reaper.SetExtState("reaticulate", "command", "set_default_channel=" .. tostring(val), false)
